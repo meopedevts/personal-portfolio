9f600cbe21de6cf184f0760c2fd7db47a1726de9
meopedevts <marcelo.meope@icloud.com>
2023-09-11 22:05:26 +0000

    Arquivo que faltou salvar

--- app/(routes)/contacts/page.tsx
+++ app/(routes)/contacts/page.tsx
@@ -3,9 +3,9 @@ import MediaContact from '@/components/contacts/media-contact'
 
 export default function Contacts() {
   return (
-    <main className="mx-auto flex max-w-5xl flex-col items-start justify-start px-5 xl:px-0">
-      <section className="mx-auto flex w-full max-w-5xl flex-col items-start justify-center gap-10 py-8 xl:px-0">
-        <div className="align-center flex w-full max-w-5xl flex-col justify-between gap-4 md:flex-row xl:px-0">
+    <main className="mx-auto flex max-w-5xl flex-col items-start justify-start overflow-hidden px-5 xl:px-0">
+      <section className="mx-auto flex w-full max-w-5xl flex-col items-start justify-center gap-10 overflow-hidden py-8 xl:px-0">
+        <div className="align-center flex w-full max-w-5xl flex-col justify-between gap-4 overflow-hidden md:flex-row xl:px-0">
           <AboutContact />
           <MediaContact />
         </div>
