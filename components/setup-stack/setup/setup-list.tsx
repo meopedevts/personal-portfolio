168f7deec85a5a84481c22172c920bcb47e0382b
meopedevts <marcelo.meope@icloud.com>
2023-09-15 01:33:10 +0000

    feat: Implementação de input de filtragem dos cards

--- components/setup-stack/setup/setup-list.tsx
+++ components/setup-stack/setup/setup-list.tsx
@@ -3,11 +3,13 @@
 import { getSetup } from '@/services/notion'
 import { GetSetupType } from '@/types/notion-setup-types'
 import SetupCard from '@/components/setup-stack/setup/setup-card'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import SetupSkeleton from '@/components/setup-stack/setup/setup-skeleton'
+import { Input } from '@/components/ui/input'
 
 const SetupList = () => {
   const [setupData, setsetupData] = useState<GetSetupType[]>([])
+  const [query, setQuery] = useState('')
 
   const [requestDone, setrequestDone] = useState(false)
 
@@ -16,6 +18,15 @@ const SetupList = () => {
     setsetupData(response)
     setrequestDone(true)
   }
+
+  const filteredSetup = useMemo(() => {
+    return setupData.filter((setup) => {
+      return (
+        setup.title.toLowerCase().includes(query.toLowerCase()) ||
+        setup.group.toLowerCase().includes(query.toLowerCase())
+      )
+    })
+  }, [setupData, query])
 
   useEffect(() => {
     if (!requestDone) {
@@ -39,9 +50,16 @@ const SetupList = () => {
           Lista de equipamentos que uso diariamente
         </h2>
       </div>
-      {/* <div className="mx-auto flex max-w-5xl flex-col items-center justify-center gap-6 py-6 md:mx-3 md:flex-row md:flex-wrap md:justify-start"> */}
+      <div className="flex max-w-5xl">
+        <Input
+          placeholder="Pesquise o item desejado"
+          className="dark"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
       <div className="mx-1 flex max-w-5xl flex-wrap items-center justify-between gap-5 py-6 md:mx-3 md:justify-start">
-        {setupData.map((setup, index) => (
+        {filteredSetup.map((setup, index) => (
           <SetupCard
             key={index}
             title={setup.title}
--- components/setup-stack/stack/stack-list.tsx
+++ components/setup-stack/stack/stack-list.tsx
@@ -3,19 +3,30 @@
 import { getStack } from '@/services/notion'
 import { GetStackType } from '@/types/notion-setup-types'
 import StackCard from '@/components/setup-stack/stack/stack-card'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import StackSkeleton from '@/components/setup-stack/stack/stack-skeleton'
+import { Input } from '@/components/ui/input'
 
 const StackList = () => {
-  const [setupData, setsetupData] = useState<GetStackType[]>([])
+  const [stackData, setstackData] = useState<GetStackType[]>([])
+  const [query, setQuery] = useState('')
 
   const [requestDone, setrequestDone] = useState(false)
 
   const requestSetup = async () => {
     const response = await getStack()
-    setsetupData(response)
+    setstackData(response)
     setrequestDone(true)
   }
+
+  const filteredStack = useMemo(() => {
+    return stackData.filter((stack) => {
+      return (
+        stack.title.toLowerCase().includes(query.toLowerCase()) ||
+        stack.group.toLowerCase().includes(query.toLowerCase())
+      )
+    })
+  }, [stackData, query])
 
   useEffect(() => {
     if (!requestDone) {
@@ -33,14 +44,22 @@ const StackList = () => {
 
   return (
     <div className="mx-auto flex w-full max-w-5xl flex-col gap-4 py-6">
+      <div className="mx-1 flex max-w-5xl md:mx-3">
+        <Input
+          placeholder="Pesquise o item desejado"
+          className="dark"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
       <div className="mx-1 flex max-w-5xl flex-wrap items-center justify-between gap-6 py-6 md:mx-3 md:justify-start">
-        {setupData.map((setup, index) => (
+        {filteredStack.map((stack, index) => (
           <StackCard
             key={index}
-            title={setup.title}
-            group={setup.group}
-            image={setup.image}
-            site={setup.site}
+            title={stack.title}
+            group={stack.group}
+            image={stack.image}
+            site={stack.site}
           />
         ))}
       </div>
