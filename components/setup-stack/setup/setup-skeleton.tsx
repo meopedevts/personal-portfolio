6ec30e5b500d8f682356cba8b45aecb1938bac21
meopedevts <marcelo.meope@icloud.com>
2023-09-15 01:33:56 +0000

    feat: Implementação dos inputs nas skeletons screens dos components de '/setup' '/stack'

--- components/setup-stack/setup/setup-skeleton.tsx
+++ components/setup-stack/setup/setup-skeleton.tsx
@@ -5,8 +5,11 @@ const SetupSkeleton = () => {
   return (
     <div className="flex w-full max-w-5xl flex-col items-start justify-center gap-4 py-6">
       <div className="flex max-w-5xl flex-col gap-4">
-        <Skeleton className="dark h-12 w-72" />
-        <Skeleton className="dark h-4 w-80" />
+        <Skeleton className="dark h-10 w-72" />
+        <Skeleton className="dark h-5 w-80" />
+      </div>
+      <div className="flex w-full max-w-5xl">
+        <Skeleton className="dark h-10 w-full" />
       </div>
       <div className="mx-1 flex max-w-5xl flex-wrap items-center justify-between gap-5 py-6 md:mx-3 md:justify-start">
         <Skeleton className="dark h-[280px] w-[150px] md:h-[320px] md:w-[232px]" />
--- components/setup-stack/stack/stack-skeleton.tsx
+++ components/setup-stack/stack/stack-skeleton.tsx
@@ -4,6 +4,9 @@ import React from 'react'
 const StackSkeleton = () => {
   return (
     <div className="flex w-full max-w-5xl flex-col items-start justify-center gap-4 py-6">
+      <div className="mx-1 flex w-full max-w-5xl md:mx-3">
+        <Skeleton className="dark h-10 w-[calc(100%-.5rem)] md:w-[calc(100%-1.5rem)]" />
+      </div>
       <div className="mx-1 flex max-w-5xl flex-wrap items-center justify-between gap-6 py-6 md:mx-3 md:justify-start">
         <Skeleton className="dark h-[260px] w-[150px] md:w-[232px]" />
         <Skeleton className="dark h-[260px] w-[150px] md:w-[232px]" />
