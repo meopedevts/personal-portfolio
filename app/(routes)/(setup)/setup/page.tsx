bb9cdf5b18908a29d3ec43ee40a66c70ef400c2e
meopedevts <marcelo.meope@icloud.com>
2023-09-19 22:55:49 +0000

    refactor: Refatoração das páginas das rotas '/setup' e '/stack' onde a chamada do componente client side agora passa pelo React.Suspense para que os dados estáticos da página não passem pela montagem do lado do client e se mantenham sendo renderizadas do lado do servidor

--- app/(routes)/(setup)/setup/page.tsx
+++ app/(routes)/(setup)/setup/page.tsx
@@ -1,12 +1,16 @@
+import { Suspense } from 'react'
 import SetupStackHeader from '@/components/setup-stack/setup-stack-header'
 import SetupList from '@/components/setup-stack/setup/setup-list'
+import SetupSkeleton from '@/components/setup-stack/setup/setup-skeleton'
 
 export default function Setup() {
   return (
     <main className="mx-auto flex max-w-5xl flex-col items-center justify-center overflow-hidden px-5 py-8 xl:px-0">
       <section className="mx-auto flex max-w-5xl flex-col items-center justify-center gap-4 overflow-hidden xl:px-0">
         <SetupStackHeader title="Setup" />
-        <SetupList />
+        <Suspense fallback={<SetupSkeleton />}>
+          <SetupList />
+        </Suspense>
       </section>
     </main>
   )
--- app/(routes)/(setup)/stack/page.tsx
+++ app/(routes)/(setup)/stack/page.tsx
@@ -1,12 +1,16 @@
 import SetupStackHeader from '@/components/setup-stack/setup-stack-header'
 import StackList from '@/components/setup-stack/stack/stack-list'
+import StackSkeleton from '@/components/setup-stack/stack/stack-skeleton'
+import { Suspense } from 'react'
 
 export default function Stack() {
   return (
     <main className="mx-auto flex max-w-5xl flex-col items-center justify-center overflow-hidden px-5 py-8 xl:px-0">
       <section className="mx-auto flex max-w-5xl flex-col items-center justify-center gap-4 overflow-hidden xl:px-0">
         <SetupStackHeader title="Stack" />
-        <StackList />
+        <Suspense fallback={<StackSkeleton />}>
+          <StackList />
+        </Suspense>
       </section>
     </main>
   )
